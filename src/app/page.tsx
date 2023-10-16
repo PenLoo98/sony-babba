c781705657fb1b4b77e8430e9f28bdd2a27ce4da
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-17 01:46:04 +0900

    create main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,28 @@ import Button from "@mui/material/Button";
 
 export default function Home() {
   return (
-    <div >
-      <h1>운동은 <br/>
-        함께할 때 <br/>
-        더 즐거우니까</h1>
-      <>
-        <Button variant="contained" color="success">
-           지금 시작하기
-        </Button>
-      </>
+    <div style={{display: "flex"}}>
+      <div className="left-container"
+        style={{padding: "20px"}}>
+        <h1>
+          운동은 <br />
+          함께할 때 <br />더 즐거우니까
+        </h1>
+        <>
+          <Button variant="contained" color="success">
+            지금 시작하기
+          </Button>
+        </>
+      </div>
+      <div className="right-container"
+        style={{padding: "20px"}}>
+        <Image
+          src="/main-page.png"
+          width={914}
+          height={677}
+          alt="main-page-image"
+        />
+      </div>
     </div>
   );
 }
