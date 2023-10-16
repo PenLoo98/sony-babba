35e3cd85760afaba7e4248fd92836ed8d5dbb34e
penloo <76810066+PenLoo98@users.noreply.github.com>
2023-10-17 00:59:19 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Button from "@mui/material/Button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>With Sports</h1>
+    <div >
+      <h1>운동은 <br/>
+        함께할 때 <br/>
+        더 즐거우니까</h1>
+      <>
+        <Button variant="contained" color="success">
+           지금 시작하기
+        </Button>
+      </>
     </div>
-  )
+  );
 }
