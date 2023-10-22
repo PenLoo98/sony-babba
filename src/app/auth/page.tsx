d5829bb2f09c5e1cd9c9930b9e02a560ce2a1d70
penloo <76810066+PenLoo98@users.noreply.github.com>
2023-10-22 14:35:15 +0900

    Update the auth page.tsx

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,11 +3,11 @@ import ParsingQuery from "@/components/Auth/ParsingQuery";
 import SelectArea from "@/components/Auth/SelectArea";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
-import Link from "next/link";
 import { useState } from "react";
 
 export default function SignUp() {
     // 닉네임 중복 확인 API
+    // TODO: API 주소 변경하기
     const nameCheckAPI = "http://localhost:3000/api/user/nickname";
 
     // 닉네임 입력 상태관리
@@ -21,6 +21,23 @@ export default function SignUp() {
     const handleAreaChange = (event: string) => {
         setArea(event);
     };
+
+    // 닉네임 중복 확인
+    function checkName(){
+      fetch(nameCheckAPI, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({nickname}),
+      }).then((res) => {
+        if(res.status === 200){
+          alert("사용 가능한 닉네임입니다.");
+        } else {
+          alert("이미 사용중인 닉네임입니다.");
+        }
+      })
+    }
 
     // 회원정보 제출
     function sendForm(){
@@ -36,9 +53,7 @@ export default function SignUp() {
       
       <div className="nickname" style={{ display: "flex" }}>
         <TextField label="닉네임" variant="outlined" value={nickname} onChange={handleNameChange}/>
-        <Link href={nameCheckAPI} style={{ textDecoration: "none" }}>
-        <Button variant="contained" style={{margin:"10px"}}>중복 확인</Button>
-        </Link>
+        <Button onClick={checkName}variant="contained" style={{margin:"10px"}}>중복 확인</Button>
       </div>
       <br />
       <SelectArea area={area} onAreaChange={handleAreaChange}/>
