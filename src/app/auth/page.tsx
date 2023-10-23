2605cec4f226d0ef397fcdfc204186abe187d202
penloo <76810066+PenLoo98@users.noreply.github.com>
2023-10-23 15:53:51 +0900

    Update auth page.tsx
    Rearrange the input forms of nickname, area

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -5,6 +5,7 @@ import ParsingQuery from "@/components/Auth/ParsingQuery";
 import SelectArea from "@/components/Auth/SelectArea";
 import { TextField } from "@mui/material";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function SignUp() {
   // 닉네임 입력 상태관리
@@ -20,41 +21,53 @@ export default function SignUp() {
   };
 
   return (
-    <div style={{ padding: "20px" }}>
-      <div
-        className="token"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        <ParsingQuery />
+    <div style={{ display: "flex", padding: "20px" }}>
+      <div className="signupForm">
+        <h1>같이 운동하고 싶을 때</h1>
+        <h1 style={{ color: "#248CC7" }}>With Sports</h1>
+        <br />
+        <br />
+        <div
+          className="token"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <ParsingQuery />
+        </div>
+        <br />
+        <div
+          className="inputUserInfo"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <div className="nickname">
+            <TextField
+              label="닉네임"
+              variant="outlined"
+              value={nickname}
+              onChange={handleNameChange}
+            />
+            <CheckNickButton nickname={nickname} />
+            <br />
+            <br />
+            <div className="area">
+              <SelectArea area={area} onAreaChange={handleAreaChange} />
+            </div>
+          </div>
+        </div>
+        <br />
+        <div
+          className="exportUserBtn"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <ExportUserInfoButton nickname={nickname} area={area} />
+        </div>
       </div>
-      <br />
-
-      <div
-        className="nickname"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        <TextField
-          label="닉네임"
-          variant="outlined"
-          value={nickname}
-          onChange={handleNameChange}
-        />
-        <CheckNickButton nickname={nickname} />
-      </div>
-      <br />
-      <div
-        className="area"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        <SelectArea area={area} onAreaChange={handleAreaChange} />
-      </div>
-      <br />
-      <div
-        className="exportUserBtn"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        <ExportUserInfoButton nickname={nickname} area={area} />
-      </div>
+      <Image
+        src="/auth-page.png"
+        alt="auth-page-image"
+        width={800}
+        height={800}
+        style={{ marginLeft: "50px" }}
+      />
     </div>
   );
 }
