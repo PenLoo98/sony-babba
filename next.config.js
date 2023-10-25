4b1a32e1b58f08cf3a9c4fa18893885dff0314e5
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-26 03:40:17 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,15 @@
 nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "http://3.37.203.5:8000/:path*",
-      },
-    ];
-  },
+    async rewrites() {
+      return [
+        {
+          // `/user-service/signup/check/nickname?nickname=${nickname}`
+          source: '/user-service/signup/check/nickname/:nickname*',
+          destination: 'http://3.37.203.5:8000/user-service/signup/check/nickname?nickname=:nickname*',
+        },
+      ]
+    },
 
 }
 
