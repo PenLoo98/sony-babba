6280dd70f5e04ada3996e263b3c013aa1a099970
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-26 04:53:10 +0900

    Update CheckNickButton.tsx

--- src/components/AuthComponent/CheckNickButton.tsx
+++ src/components/AuthComponent/CheckNickButton.tsx
@@ -1,4 +1,3 @@
-"use client;";
 import { Button } from "@mui/material";
 
 type CheckNickButtonProps = {
@@ -10,13 +9,14 @@ export default function CheckNickButton({
   nickname,
   setValidName,
 }: CheckNickButtonProps) {
+  console.log("nickname: " + nickname);
   // 닉네임 중복 확인 API
   const nameCheckAPI = `http://3.37.203.5:8000/user-service/signup/check/nickname?nickname="${nickname}"`;
 
   // 액세스 토큰 가져오기
   const localStorage: Storage = window.localStorage;
   const token = localStorage.getItem("accessToken");
-  const tokenJSON = {accessToken: token};
+  const tokenJSON = { accessToken: token };
   // console.log(token);
   // console.log(tokenJSON);
 
@@ -33,24 +33,23 @@ export default function CheckNickButton({
   // }
 
   // 닉네임 중복 확인
-  function checkName() {
-    fetch(nameCheckAPI, {
+  async function checkName() {
+    const res = await fetch(nameCheckAPI, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${tokenJSON}`,
       },
-    }).then((res) => {
-      if (res.status === 200) {
-        alert("사용 가능한 닉네임입니다.");
-        console.log(res);
-        setValidName(true);
-      } else {
-        alert("이미 사용중인 닉네임입니다.");
-        console.log(res);
-        setValidName(false);
-      }
     });
+    if (!res.ok) {
+      alert("이미 사용중인 닉네임입니다.");
+      setValidName(false);
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("서버 요청 실패!");
+    }
+    setValidName(true);
+    alert("사용 가능한 닉네임입니다.");
+    return res.json();
   }
 
   return (
