4ca561ffab803f7b78257a9d049d55025b443b99
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-25 18:18:45 +0900

    Update LoginTest.tsx

--- src/components/Headers/LoginTest.tsx
+++ src/components/Headers/LoginTest.tsx
@@ -1,39 +1,30 @@
 "use client";
 import { Button } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LoginTest() {
-  function requestLogin() {
-    // const ip = process.env.SERVER_IP;
-    // const url = `http://54.180.109.176:8000/user-service/oauth2/authorization/naver`;
-    const url = `http://3.37.203.5:8000/user-service/oauth2/authorization/naver`;
-    const authPopup = window.open(url, "authPopup", "width=800, height=600");
+  // const ip = process.env.SERVER_IP;
+  // const port = process.env.SERVER_PORT;
+  const url: string =
+    "http://3.37.203.5:8000/user-service/oauth2/authorization/naver";
     
-    // 쿼리 응답을 받음
-    // fetch(url, {
-    //   method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // }).then((res) => {
-    //   // 액세스 토큰 로컬 스토리지에 저장
-    //   localStorage.setItem("accessToken", res.data.accessToken);
-    // });
-    }
-    return (
-      <Button
-        onClick={requestLogin}
-        startIcon={
-          <Image src="/naver.png" alt="naver" width={50} height={50} />
-        }
-        style={{
-          background: "#27D34A",
-          color: "black",
-          margin: "20px",
-        }}
-      >
-        네이버 로그인 테스트
-      </Button>
-    );
-  
+  return (
+    <div className="loginNaver">
+      <Link href={url}>
+        <Button
+          startIcon={
+            <Image src="/naver.png" alt="naver" width={50} height={50} />
+          }
+          style={{
+            background: "#27D34A",
+            color: "black",
+            margin: "20px",
+          }}
+        >
+          네이버 로그인 테스트
+        </Button>
+      </Link>
+    </div>
+  );
 }
