eb845543e6cd16553a3b19e76223fe4176b3a9e3
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-25 22:39:20 +0900

    Update ExportUserInfoButton.tsx

--- src/app/auth/authComponent/ExportUserInfoButton.tsx
+++ src/app/auth/authComponent/ExportUserInfoButton.tsx
@@ -13,6 +13,30 @@ export default function ExportUserInfoButton(props: ExportUserInfoProps) {
   function sendForm() {
     // TODO: 회원정보 제출 fetch 구현하기
     console.log(JSON.stringify({ nickname, area }));
+
+    // 회원정보 제출 API
+    const url = " http://3.37.203.5:8000/user-service/signup/profile";
+
+    // 액세스 토큰 가져오기
+    const localStorage: Storage = window.localStorage;
+    const token = localStorage.getItem("accessToken");
+
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ nickname, area }),
+    }).then((res) => {
+      if (res.status === 200) {
+        alert("회원가입에 성공했습니다.");
+        console.log(res);
+      } else {
+        alert("회원가입에 실패했습니다.");
+        console.log(res);
+      }
+    });
   }
   return (
     <div>
