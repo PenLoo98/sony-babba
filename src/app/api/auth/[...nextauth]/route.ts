af1290ca17054594a7559a02c8778208c6ad399c
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-26 11:45:37 +0900

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,17 @@
-import NextAuth from "next-auth/next";
-import {AuthOptions} from "next-auth";
-import NaverProvider from "next-auth/providers/naver";
+// import NextAuth from "next-auth/next";
+// import {AuthOptions} from "next-auth";
+// import NaverProvider from "next-auth/providers/naver";
 
-export const authOptions: AuthOptions = {
-    providers: [
-        NaverProvider({
-            clientId: process.env.NAVER_CLIENT_ID ?? "",
-            clientSecret: process.env.NAVER_CLIENT_SECRET ?? "",
-        }),
-    ],
-};
+// export const authOptions: AuthOptions = {
+//     providers: [
+//         NaverProvider({
+//             clientId: process.env.NAVER_CLIENT_ID ?? "",
+//             clientSecret: process.env.NAVER_CLIENT_SECRET ?? "",
+//         }),
+//     ],
+// };
 
 
-export const handler = NextAuth(authOptions);
+// export const handler = NextAuth(authOptions);
 
-export {handler as GET, handler as POST};
+// export {handler as GET, handler as POST};
