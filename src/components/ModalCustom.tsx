c5a5f9f92d20d2ffdd034fb1203122dd1d6fd63a
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-26 11:38:46 +0900

    Update ModalCustom.tsx

--- src/components/ModalCustom.tsx
+++ src/components/ModalCustom.tsx
@@ -1,35 +1,43 @@
 "use client";
-import { Modal,Box} from "@mui/material";
+import { Modal, Box } from "@mui/material";
 import { useState } from "react";
 
 type ModalProps = {
-    children: React.ReactNode;
-  };
+  children: React.ReactNode;
+  show: boolean;
+  setShow: (event: boolean) => void;
+};
 
-export default function ModalCustom({ children }: ModalProps) {
-    const [show, setShow] = useState(false);
+export default function ModalCustom({ children, show, setShow }: ModalProps) {
+  // 부모 클래스에 추가할 것
+  // 프로필 수정 모달
+  // const [show, setShow] = useState(false);
+  // const editProfile = () => {
+  //   setShow(true);
+  // }
 
-    return (        
-        <Modal
-                open={show}
-                onClose={() => setShow(false)}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-              >
-                <div
-                  className="edit-profile"
-                  style={{
-                    backgroundColor: "white",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    margin: "150px",
-                  }}
-                >
-                  <Box style={{ margin: "30px" }}>
-                    {children}
-                  </Box>
-                </div>
-              </Modal>
-    )
+  // 컴포넌트 추가
+  // <ModalCustom show={show} setShow={setShow}></ModalCustom>
+
+  return (
+    <Modal
+      open={show}
+      onClose={() => setShow(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <div
+        className="edit-profile"
+        style={{
+          backgroundColor: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "150px",
+        }}
+      >
+        <Box style={{ margin: "30px" }}>{children}</Box>
+      </div>
+    </Modal>
+  );
 }
