b21a5846c8b3b81070f7be2a21ec24e03363fa80
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-26 18:05:17 +0900

    edit CORS configuration

--- next.config.js
+++ next.config.js
@@ -2,16 +2,20 @@
 nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    // asyn header()
-    // async redirects(){
-    //   return [
-    //     {
-    //       source: 'https://sony-babba.vercel.app/sapi/:path*',
-    //       destination: 'http://3.37.203.5:8000/:path*',
-    //     }
-    //   ]
-    // }
-
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    }
 }
 
 module.exports = nextConfig
