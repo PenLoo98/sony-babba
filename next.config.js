9084e3304023e9cd2187bbdcc341d9a73108b74d
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-26 16:55:08 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ nextConfig = {
       return [
         {
           // `/user-service/signup/check/nickname?nickname=${nickname}`
-          source: 'https://sony-babba.vercel.app/user-service/signup/check/nickname/:nickname*',
+          source: 'https://sony-babba.vercel.app/user-service/signup/check/nickname?:nickname*',
           destination: 'http://3.37.203.5:8000/user-service/signup/check/nickname?nickname=:nickname*',
         },
       ]
