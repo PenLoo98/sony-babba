3bb31952ddf36a4b8af3c543b6786127bccd91c8
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-27 20:26:08 +0900

    add the team main page

---
+++ src/app/team/[id]/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import ModalCustom from "@/components/ModalCustom";
+export default function TeamSpecific() {
+  // 검색할 팀/팀원 명
+  const [checkName, setCheckname] = useState("");
+  const typeName = (e: any) => {
+    setCheckname(e.target.value);
+  };
+  const searchName = (e: any) => {
+    e.preventDefault();
+    // 입력값 확인
+    alert("입력한 닉네임: " + checkName);
+  };
+
+  // 팀 생성 모달
+  const [show, setShow] = useState(false);
+  const createTeam = () => {
+    setShow(true);
+  };
+
+  return (
+    <div className="teamSpecific">
+      <Image src="/team-main.png" alt="team" width={180} height={180} />
+
+      <div className="team-search">
+        <TextField
+          id="outlined-basic"
+          label="팀/팀원 검색"
+          variant="outlined"
+          value={checkName}
+          onChange={typeName}
+        />
+        <Image
+          onClick={searchName}
+          src="/search.png"
+          alt="search"
+          width={40}
+          height={40}
+          style={{ margin: "10px 0 0 10px" }}
+        />
+      </div>
+      <Button variant="outlined" onClick={createTeam}>
+        팀 생성
+      </Button>
+      <ModalCustom show={show} setShow={setShow}>
+        <h1>팀 생성</h1>
+      </ModalCustom>
+      
+      <h1>팀원 구인</h1>
+      <h2>손이바빠: 판교에서 같이 축구하실 분들 구합니다.</h2>
+    </div>
+  );
+}
