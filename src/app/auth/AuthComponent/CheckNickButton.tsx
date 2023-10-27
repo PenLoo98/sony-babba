7596a6ffcd00b6797ebc3526ba788b5a579ea370
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-10-28 02:20:40 +0900

    Test 2: change the query parameter format

--- src/app/auth/AuthComponent/CheckNickButton.tsx
+++ src/app/auth/AuthComponent/CheckNickButton.tsx
@@ -38,58 +38,57 @@ export default function CheckNickButton({
 
     // 닉네임 중복 확인 API
     // const nameCheckAPI = `https://withsports.shop:8000/user-service/signup/check/nickname`;
-    const nameCheckAPI = `https://withsports.shop:8000/user-service/signup/check/nickname/?nickname=${nickname}`;
-    const encodedURL = encodeURI(nameCheckAPI + `?nickname=${nickname}`);
+    const nameCheckAPI = `https://withsports.shop:8000/user-service/signup/check/nickname/`;
 
-    // fetch API
-    let res = await fetch(nameCheckAPI, {
-      method: "GET",
-      headers: {
-        Credentials: "include",
-        ContentType: "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          alert("사용 가능한 닉네임입니다.");
-          setValidName(true);
-        } else {
-          alert("이미 사용중인 닉네임입니다.");
-          setValidName(false);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        throw new Error("서버 요청 실패!");
-      });
+    // // fetch API
+    // let res = await fetch(nameCheckAPI+`?nickname=${nickname}`, {
+    //   method: "GET",
+    //   headers: {
+    //     Credentials: "include",
+    //     ContentType: "application/json",
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    // })
+    //   .then((res) => {
+    //     if (res.status === 200) {
+    //       alert("사용 가능한 닉네임입니다.");
+    //       setValidName(true);
+    //     } else {
+    //       alert("이미 사용중인 닉네임입니다.");
+    //       setValidName(false);
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //     throw new Error("서버 요청 실패!");
+    //   });
 
     // axios API
     // params: {nickname }, // 이게 맞는 표현
 
-    // axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false });
-    // axios
-    //   .get(nameCheckAPI, {
-    //     params: {nickname},
-    //     headers: {
-    //       Credentials: "include",
-    //       ContentType: "application/json",
-    //       Authorization: `Bearer ${token}`,
-    //     },
-    //   })
-    //   .then((res) => {
-    //     console.log(res);
-    //     if (res.data === true) {
-    //       alert("이미 사용중인 닉네임입니다.");
-    //       setValidName(false);
-    //     } else {
-    //       alert("사용 가능한 닉네임입니다.");
-    //       setValidName(true);
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
+    axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false });
+    axios
+      .get(nameCheckAPI, {
+        params: {nickname: nickname},
+        headers: {
+          Credentials: "include",
+          ContentType: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          alert("이미 사용중인 닉네임입니다.");
+          setValidName(false);
+        } else {
+          alert("사용 가능한 닉네임입니다.");
+          setValidName(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   return (
