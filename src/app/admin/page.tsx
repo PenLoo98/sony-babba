0bb5d7122a45ec05435e7e84ebe723fa080cd142
JokerBear_96 <9619pjw@gmail.com>
2023-10-28 15:57:32 +0900

    admin page sample

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Button from "@mui/material/Button";
+import Link from "next/link";
+import Box from '@mui/material/Box';
+
+export default function Admin() {
+    const buttons = [
+        { name: 'Dashboard', imgSrc: '/dashboard.png' },
+        { name: 'Member', imgSrc: '/member.png' },
+        { name: 'Team', imgSrc: '/team.png' },
+        { name: 'Book', imgSrc: '/book.png' },
+        { name: 'Match', imgSrc:'/match.png'},
+        { name: 'Post', imgSrc:'/post.png'},
+        { name: 'Gift', imgSrc:'/gift.png'},
+        { name: 'Report', imgSrc:'/report.png'}
+    ];
+
+    return (
+        <Box sx={{
+            display:"grid",
+            gap:"20px",
+            gridTemplateColumns:{
+            xs:"repeat(1, 1fr)",
+            sm:"repeat(2, 1fr)",
+            md:"repeat(4, 1fr)"
+            }
+        }}>
+        {
+            buttons.map((button,i) => 
+                <div key={i} className="left-container" style={{ padding:"20px" }}>
+                    <Link href={`/admin/${button.name.toLowerCase()}`} style={{ textDecoration: "none" }}>
+                        <Button style={{ textTransform: 'none', flexDirection:"column", display:'flex', justifyContent:'center'}}>
+                            <Image src={button.imgSrc}
+                                width={200}
+                                height={200}
+                                alt={`${button.name}-image`}/>
+                            <Box sx={{
+                                width : '200px',
+                                height : '70px',
+                                border:'2px',
+                                borderRadius:'8px',
+                                padding:'5px',
+                                marginTop:'10px'
+                                }}>
+                                <h2>{button.name}</h2>
+                            </Box>
+                        </Button>
+                    </Link>
+                </div>
+                )
+            }
+        </Box>
+    );
+}
