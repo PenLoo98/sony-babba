e23ec25611e9fd5d81779a436f6bf0a87fa7c699
penloo <76810066+PenLoo98@users.noreply.github.com>
2023-10-30 20:41:41 +0900

    add Redirect in the auth page.tsx

--- src/app/auth/AuthComponent/ParsingQuery.tsx
+++ src/app/auth/AuthComponent/ParsingQuery.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 // 리다이렉트 URI에 붙은 쿼리를 파싱하는 컴포넌트
 export default function ParsingQuery() {
   const params = useSearchParams();
+  const router = useRouter();
 
   const accessToken: any | null = params.get("accessToken");
   const expiredTime: any | null = params.get("expiredTime");
@@ -19,6 +20,9 @@ export default function ParsingQuery() {
       localStorage.setItem("expiredTime", expiredTime);
       localStorage.setItem("userId", userId);
     }
+
+    // 데이터를 저장한 후 "/"로 리다이렉트합니다.
+    router.push('/');
   }, []);
 
   return (
