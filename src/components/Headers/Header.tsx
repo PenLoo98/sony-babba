fd547aa5275a6ff96960afbe17026a5642aed1d1
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-11-02 02:35:49 +0900

    feat Header/MenuBar

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -3,11 +3,12 @@ import { useEffect, useState } from "react";
 import HomeButton from "./HomeButton";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
+import MenuBar from "./MenuBar";
 export default function Header() {
   const homeButtonStyle = {
     display: "flex",
-        justifyContent: "space-between",
-        backgroundColor: "#85f3ef"
+    justifyContent: "space-between",
+    backgroundColor: "#85f3ef",
   };
   const [isLogin, setIsLogin] = useState(false);
   useEffect(() => {
@@ -16,22 +17,22 @@ export default function Header() {
     const expiredTime = localStorage.getItem("expiredTime");
     if (token && expiredTime) {
       setIsLogin(true);
-    }
-    else{
+    } else {
       setIsLogin(false);
     }
   }, []);
 
   return (
-    <div
-      className="header"
-      style={homeButtonStyle}
-    >
+    <div className="header" style={homeButtonStyle}>
       <HomeButton />
-      {isLogin ? (<LoginButton />) : (<LogoutButton/>) }
-      
-      {/* <LogInOutNaver /> */}
-      {/* <LogoutBtn /> */}
+      {isLogin ? (
+        <div>
+          <MenuBar />
+          <LoginButton />
+        </div>
+      ) : (
+        <LogoutButton />
+      )}
     </div>
   );
 }
