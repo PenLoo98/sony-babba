58fa1fd9ddf2412002539ebc775530044bb8f2f9
penloo <76810066+PenLoo98@users.noreply.github.com>
2023-11-01 14:58:43 +0900

    Test 15: /team/[id] response parsing

--- src/app/team/[id]/page.tsx
+++ src/app/team/[id]/page.tsx
@@ -13,12 +13,11 @@ export default function ShowTeam({ params }: { params: PageParams }) {
   const token = localStorage.getItem("accessToken");
 
   const [showTeamInfo, setShowTeamInfo] = useState(false);
+  const [data, setData] = useState({});
+
   // TODO: 응답값에서 팀 정보 가져오기
   useEffect(() => {
-    const data = getTeamInfo(getTeamInfoURL);
-    // console.log(data);
-    // console.log(JSON.stringify(data));
-    // console.log(JSON.parse(JSON.stringify(data)));
+    getTeamInfo(getTeamInfoURL);
   }, []);
 
   async function getTeamInfo(getTeamInfoURL: string) {
@@ -30,24 +29,22 @@ export default function ShowTeam({ params }: { params: PageParams }) {
         Authorization: `Bearer ${token}`,
       },
     })
-      .then(res => res.text())
-        // // 응답이 성공적으로 왔을 때
-        // console.log(res);
-        // console.log(res.body);
-        // console.log(JSON.stringify(res.body));
-        // console.log(JSON.parse(JSON.stringify(res.body)));
-        
-        // if (res.status === 200) {
-        //   console.log("팀 정보 조회에 성공했습니다.");
-        //   setShowTeamInfo(true);
-        //   return res;
-        // } else {
-        //   console.log("팀 정보 조회에 실패했습니다.");
-        //   console.log(res);
-        //   setShowTeamInfo(false);
-        // }
-      // })
-      .then(data => {console.log(data); return data;})
+      .then((res) => {
+        // 응답이 성공적으로 왔을 때
+        if (res.status === 200) {
+          console.log("팀 정보 조회에 성공했습니다.");
+          setShowTeamInfo(true);
+        } else {
+          console.log("팀 정보 조회에 실패했습니다.");
+          console.log(res);
+          setShowTeamInfo(false);
+          return null;
+        }
+        res.text();
+      })
+      .then((data) => {
+        return data;
+      })
       .catch((error) => {
         console.log(error);
         throw new Error("서버 요청 실패!");
@@ -57,6 +54,7 @@ export default function ShowTeam({ params }: { params: PageParams }) {
     if (response) {
       body = await JSON.parse(response);
       console.log(body);
+      setData(body);
       return body;
     } else {
       return null;
@@ -66,20 +64,21 @@ export default function ShowTeam({ params }: { params: PageParams }) {
   return (
     <div>
       <h1>팀 정보를 불러오는 중입니다...</h1>
-      {/* {!showTeamInfo && <div>팀 정보를 불러오는 중입니다...</div>} */}
-      {/* {showTeamInfo && (
+      {!showTeamInfo && <div>팀 정보를 불러오는 중입니다...</div>}
+      {showTeamInfo && (
         <div>
           <h1>팀 정보를 불러오는데 성공했습니다.</h1>
-          <Image src={data.imageUrl} alt="팀 이미지" width={200} height={200} />
+          <Image src={data.data.imageUrl} alt="팀 이미지" width={200} height={200} />
           <h1>팀 페이지</h1>
-          <h2>팀 이름: {data.teamName}</h2>
-          <h2>팀 소개: {data.introduction}</h2>
-          <h2>팀 지역: {data.area}</h2>
-          <h2>팀 스포츠: {data.sports}</h2>
-          <h2>팀 이미지: {data.image}</h2>
-          <h2>팀 인원 수: {data.teamMemberCount}</h2>
+          <h2>팀 이름: {data.data.teamName}</h2>
+          <h2>팀장 이름: {data.data.leaderNickname}</h2>
+          <h2>팀 소개: {data.data.introduction}</h2>
+          <h2>팀 지역: {data.data.area}</h2>
+          <h2>팀 종목: {data.data.sports}</h2>
+          <h2>팀 인원 수: {data.data.teamMemberCount}</h2>
+          <h2>팀원 목록: {data.data.pageable}</h2>
         </div>
-      )} */}
+      )}
     </div>
   );
 }
