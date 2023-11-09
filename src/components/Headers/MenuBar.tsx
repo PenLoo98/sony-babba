d902b59a13c72f65b94298d0ccdd53842f4f13f1
JokerBear_96 <9619pjw@gmail.com>
2023-11-09 12:37:14 +0900

    Add vote Button

--- src/components/Headers/MenuBar.tsx
+++ src/components/Headers/MenuBar.tsx
@@ -2,64 +2,73 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function MenuBar() {
-    return(
-        <div className="menuBar"
-        style={{display: "flex"}}>
-            <Link href="/post" style={{ textDecoration: "none" }}>
-              <Image
-                src="/forumBtn.png"
-                width={70}
-                height={70}
-                alt="forum-image"
-                style={{ margin: "0 20px" }}
-              />
-            </Link>
-            <Link href="/matching/main" style={{ textDecoration: "none" }}>
-              <Image
-                src="/versus.png"
-                width={70}
-                height={70}
-                alt="team-image"
-                style={{ margin: "0 20px" }}
-              />
-            </Link>
-            <Link href="/team/main" style={{ textDecoration: "none" }}>
-              <Image
-                src="/teamBtn.png"
-                width={70}
-                height={70}
-                alt="team-image"
-                style={{ margin: "0 20px" }}
-              />
-            </Link>
-            <Link href="/chat/main" style={{ textDecoration: "none" }}>
-              <Image
-                src="/chatBtn.png"
-                width={70}
-                height={70}
-                alt="chat-image"
-                style={{ margin: "0 20px" }}
-              />
-            </Link>
-            <Link href="/alarm/main" style={{ textDecoration: "none" }}>
-              <Image
-                src="/bell.png"
-                width={70}
-                height={70}
-                alt="chat-image"
-                style={{ margin: "0 20px" }}
-              />
-            </Link>
-            <Link href="/user/profile/main" style={{ textDecoration: "none" }}>
-              <Image
-                src="/profileBtn.png"
-                width={70}
-                height={70}
-                alt="profile-image"
-                style={{ margin: "0 20px" }}
-              />
-            </Link>
-            <hr />
-        </div>
-    );
+  return (
+    <div className="menuBar" style={{ display: "flex" }}>
+      <Link href="http://3.39.154.148:3000/" style={{ textDecoration: "none" }}>
+        <Image
+          src="/vote.png"
+          width={70}
+          height={70}
+          alt="vote-image"
+          style={{ margin: "0 20px" }}
+        />
+      </Link>
+      <Link href="/post" style={{ textDecoration: "none" }}>
+        <Image
+          src="/forumBtn.png"
+          width={70}
+          height={70}
+          alt="forum-image"
+          style={{ margin: "0 20px" }}
+        />
+      </Link>
+      <Link href="/matching/main" style={{ textDecoration: "none" }}>
+        <Image
+          src="/versus.png"
+          width={70}
+          height={70}
+          alt="team-image"
+          style={{ margin: "0 20px" }}
+        />
+      </Link>
+      <Link href="/team/main" style={{ textDecoration: "none" }}>
+        <Image
+          src="/teamBtn.png"
+          width={70}
+          height={70}
+          alt="team-image"
+          style={{ margin: "0 20px" }}
+        />
+      </Link>
+      <Link href="/chat/main" style={{ textDecoration: "none" }}>
+        <Image
+          src="/chatBtn.png"
+          width={70}
+          height={70}
+          alt="chat-image"
+          style={{ margin: "0 20px" }}
+        />
+      </Link>
+      <Link href="/alarm/main" style={{ textDecoration: "none" }}>
+        <Image
+          src="/bell.png"
+          width={70}
+          height={70}
+          alt="chat-image"
+          style={{ margin: "0 20px" }}
+        />
+      </Link>
+      <Link href="/user/profile/main" style={{ textDecoration: "none" }}>
+        <Image
+          src="/profileBtn.png"
+          width={70}
+          height={70}
+          alt="profile-image"
+          style={{ margin: "0 20px" }}
+        />
+      </Link>
+
+      <hr />
+    </div>
+  );
 }
