4700f825cb8a8e3f1c5f5901d766611c90040809
JokerBear_96 <9619pjw@gmail.com>
2023-11-10 11:22:20 +0900

    test commit

--- src/app/user/profile/main/page.tsx
+++ src/app/user/profile/main/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 
+
 type UserData = {
   userId : number;
   email : string;
@@ -9,25 +10,15 @@ type UserData = {
   address : string | null
 }
 
+export default function ProfilePage() {
+  const localStorage: Storage = window.localStorage;
+  const token = localStorage.getItem("accessToken");
 
-export default function UserProfileMain() {
-  // TODO: 유저 프로필 메인 페이지 구현하기
-  // TODO: 자신의 userId에 맞는 /user/profile/:userId 페이지로 이동하기
-  const [userData, setUserData] = useState<UserData | null>(null);
-  
-  
-  // useEffect(() => {
-  //   const localStorage = window.localStorage;
-  //   const userId = localStorage.getItem("userId");
-  //   location.href = `/user/profile/${userId}`;
-  // }, []);
+  const [showUserInfo, setShowUserInfo] = useState(false);
+  const [data, setData] = useState<UserData | null>(null);
 
   useEffect(() => {
-    const localStorage = window.localStorage;
-    const userId = localStorage.getItem("userId");
-    const token = localStorage.getItem("accessToken");
-
-    fetch(`https://withsports.shop:8000/user-service/user`, {
+    fetch('https://withsports.shop:8000/user-service/user/', {
       method: "GET",
       headers: {
         Credentials: "include",
@@ -38,10 +29,8 @@ export default function UserProfileMain() {
       .then((res) => res.json())
       .then((data) => {
         if (data.code === "SUCCESS") {
-          console.log("사용자 정보를 불러오는데 성공했습니다.");
-          setUserData(data.data);
-        } else {
-          console.log("사용자 정보를 불러오는데 실패했습니다.");
+          const userId = data.data.userId;
+          fetchUserInfo(userId);
         }
       })
       .catch((error) => {
@@ -50,17 +39,51 @@ export default function UserProfileMain() {
       });
   }, []);
 
+  const fetchUserInfo = async (userId: number) => {
+    const response = await fetch(`https://withsports.shop:8000/user-service/user/profile/${userId}`, {
+      method: "GET",
+      headers: {
+        Credentials: "include",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (data.code === "SUCCESS") {
+      setData(data.data);
+      setShowUserInfo(true);
+    } else {
+      console.log("사용자 정보를 불러오는데 실패했습니다.");
+    }
+  };
+
   return (
     <div>
       <h1>유저 프로필 메인 페이지</h1>
-      {userData && (
+      {data && (
         <div>
-          <h1>name: {userData.name}</h1>
-          <h1>email: {userData.email}</h1>
-          <h1>nickname: {userData.nickname}</h1>
-          <h1>address: {userData.address}</h1>
+          <h1>userId: {data.userId}</h1>
+          <h1>email: {data.email}</h1>
+          <h1>name: {data.name}</h1>
+          <h1>nickname: {data.nickname}</h1>
+          <h1>address: {data.address}</h1>
         </div>
       )}
     </div>
   );
 }
+
+
+// export default function UserProfileMain() {
+//   // TODO: 유저 프로필 메인 페이지 구현하기
+//   // TODO: 자신의 userId에 맞는 /user/profile/:userId 페이지로 이동하기
+//   useEffect(() => {
+//     const localStorage = window.localStorage;
+//     const userId = localStorage.getItem("userId");
+//     location.href = `/user/profile/${userId}`;
+//   }, []);
+
+//   return <h1>유저 프로필 메인 페이지</h1>;
+// }
