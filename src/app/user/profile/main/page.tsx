99eb7c798ab0a9244bf6b28a51a87024e8cd1f1f
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-11-13 13:54:02 +0900

    fix (user/profile/main page.tsx)

--- src/app/user/profile/main/page.tsx
+++ src/app/user/profile/main/page.tsx
@@ -1,28 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
-type UserData = {
-  userId : number;
-  email : string;
-  name : string;
-  nickname : string | null;
-  address : string | null
-}
-
+// 자신의 userId에 맞는 /user/profile/:userId 페이지로 이동하기
 export default function ProfilePage() {
-
-  // TODO: 유저 프로필 메인 페이지 구현하기
-  // TODO: 자신의 userId에 맞는 /user/profile/:userId 페이지로 이동하기
-  const localStorage: Storage = window.localStorage;
-  const token = localStorage.getItem("accessToken");
-  
-  // useEffect(() => {
-  //   const localStorage = window.localStorage;
-  //   const userId = localStorage.getItem("userId");
-  //   location.href = `/user/profile/${userId}`;
-  // }, []);
-
-
   useEffect(() => {
     const localStorage = window.localStorage;
     const userId = localStorage.getItem("userId");
