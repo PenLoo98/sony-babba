eed367591d6a37f458db9bdeb83aeda4a8be1569
JokerBear_96 <9619pjw@gmail.com>
2023-11-16 03:36:27 +0900

    edit image url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    images: {
+        domains: ['with-sports-s3.s3.ap-northeast-2.amazonaws.com'],
+    },
 }
 
 module.exports = nextConfig
