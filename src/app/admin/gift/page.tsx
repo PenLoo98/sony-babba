107e5ae182174164c791655ae9b00b2af218a3ff
JokerBear_96 <9619pjw@gmail.com>
2023-11-15 17:06:13 +0900

    edit Json Blob

--- src/app/admin/gift/page.tsx
+++ src/app/admin/gift/page.tsx
@@ -75,12 +75,14 @@ export default function GifticonPage() {
       // 이미지 추가 
       const formData = new FormData();
 
-      formData.append('registerGifticonRequest', JSON.stringify({
+      formData.append('registerGifticonRequest', new Blob([JSON.stringify({
         categoryName : form.categoryName,
         gifticonName : form.gifticonName,
         description : form.description,
         price : form.price,
         amount : form.amount
+      })], {
+        type : "application/json"
       }));
 
       if(image){
