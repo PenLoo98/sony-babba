fc3b1ecf241655ff2d1cf1d5351e95a05a299afb
JokerBear_96 <9619pjw@gmail.com>
2023-11-17 14:15:47 +0900

    fix2 modify modal

--- src/app/admin/gift/page.tsx
+++ src/app/admin/gift/page.tsx
@@ -202,7 +202,7 @@ export default function GifticonPage() {
       return;
     }
 
-    if (!form.categoryName ||!form.gifticonName || !form.description || !form.price || !form.amount) {
+    if (isEditing && (!form.categoryName ||!form.gifticonName || !form.description || !form.price || !form.amount)) {
       alert("모든 항목을 입력해주세요.");
       return;
     }
