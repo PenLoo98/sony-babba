4f1dd6888e426b621ee9405b4ac4cc976f36d726
JokerBear_96 <9619pjw@gmail.com>
2023-11-17 19:07:12 +0900

    add gifticon order page

--- src/app/gift/order/page.tsx
+++ src/app/gift/order/page.tsx
@@ -8,11 +8,11 @@ import Image from "next/image";
 // 기프티콘 구매 주문 요청
 // (POST) /gifticon-service/order/gifticon 
 
-
 // 주문 정보
 type OrderInfo = {
     gifticonId : number;     // 기프티콘 ID
-    toUserNickName : String; // 기프티콘을 받는 사용자의 ID
+    toUserId : number;       // 기프티콘을 받는 사용자의 ID
+    toUserNickName : string; // 기프티콘을 받는 사용자의 닉네임
     amount : number;         // 기프티콘 주문 수량
     letter : string;         // 기프티콘에 담을 편지
 }
@@ -25,9 +25,71 @@ type UserInfo = {
 };
 
 
-
-
 export default function GiftOrderPage(){
+    const [orderInfo, setOrderInfo] = useState<OrderInfo>({
+        gifticonId: 0,
+        toUserId: 0,
+        toUserNickName: "",
+        amount: 0,
+        letter: "",
+    });
 
-    return <>기프티콘 주문 화면</>;
+    const router = useRouter();
+    
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setOrderInfo({ ...orderInfo, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const localStorage: Storage = window.localStorage;
+        const token = localStorage.getItem("accessToken");
+
+        if (!token) {
+            alert("권한이 없습니다.");
+            router.push("/admin");
+            return;
+        }
+
+        try {
+            const response = await fetch("https://withsports.shop:8000/gifticon-service/order/gifticon", {
+                method: "POST",
+                headers: {
+                    Credentials: "include",
+                    ContentType: "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify(orderInfo),
+            });
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            const data = await response.json();
+
+            if (data.code === "SUCCESS") {
+                alert(data.message);
+                // 주문 성공 후 원하는 페이지로 이동
+                router.push("/somewhere");
+            } else {
+                alert("주문에 실패하였습니다.");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="number" name="gifticonId" value={orderInfo.gifticonId} onChange={handleChange} placeholder="기프티콘 ID" required />
+            <input type="number" name="toUserId" value={orderInfo.toUserId} onChange={handleChange} placeholder="기프티콘 받을 사용자의 ID" required />
+            <input type="text" name="toUserNickName" value={orderInfo.toUserNickName} onChange={handleChange} placeholder="기프티콘 받을 사용자의 닉네임" required />
+            <input type="number" name="amount" value={orderInfo.amount} onChange={handleChange} placeholder="기프티콘 주문 수량" required />
+            <textarea name="letter" value={orderInfo.letter} onChange={handleChange} placeholder="기프티콘에 담을 편지" required />
+            <button type="submit">선물하기</button>
+        </form>
+    );
 }
