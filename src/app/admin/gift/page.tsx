3c34df461eb9ce380c80967a8d925ed53204592a
JokerBear_96 <9619pjw@gmail.com>
2023-11-17 18:22:23 +0900

    restore GG

--- src/app/admin/gift/page.tsx
+++ src/app/admin/gift/page.tsx
@@ -201,7 +201,6 @@ export default function GifticonPage() {
     setTimeout(() => {
       setIsEditing(true);
     }, 0);
-    console.log(isEditing);
   };
 
   const handleUpdateSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -279,7 +278,7 @@ export default function GifticonPage() {
     }
 
     setModalOpen(false);
-    // setIsEditing(false);
+    setIsEditing(false);
   };
 
   // TODO :  삭제 ... (DELETE) /gifticon-service/gifticon/${gifticonId} => 삭제하시겠습니까? 알림 이후 진행
