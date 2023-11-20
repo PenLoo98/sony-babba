16dbb89def5b03585a7b49194d29def639fe83d8
JokerBear_96 <9619pjw@gmail.com>
2023-11-20 18:27:16 +0900

    edit received page

--- src/app/gift/received/page.tsx
+++ src/app/gift/received/page.tsx
@@ -49,107 +49,103 @@ type ReceivedGiftInfo = {
 
 export default function ReceivedGiftPage(){
 
-  return <>임시</>;
-    // const [receivedGifts, setReceivedGifts] = useState<ReceivedGiftInfo[]>([]);
-    // const [selectedGift, setSelectedGift] = useState<ReceivedGiftInfo | null>(null);
-    // const [modalOpen, setModalOpen] = useState(false);
-    // const router = useRouter();
+    const [receivedGifts, setReceivedGifts] = useState<ReceivedGiftInfo[]>([]);
+    const [selectedGift, setSelectedGift] = useState<ReceivedGiftInfo | null>(null);
+    const [modalOpen, setModalOpen] = useState(false);
+    const router = useRouter();
   
-    // useEffect(() => {
-    //   const fetchReceivedGifts = async () => {
-    //     const localStorage: Storage = window.localStorage;
-    //     const token = localStorage.getItem("accessToken");
-    //     try {
-    //       const response = await fetch("https://withsports.shop:8000/gifticon-service/order/gifticon/list/received",
-    //         {
-    //           method: "GET",
-    //           headers: {
-    //             Credentials: "include",
-    //             ContentType: "application/json",
-    //             Authorization: `Bearer ${token}`,
-    //           },
-    //         }
-    //       );
+    useEffect(() => {
+      const fetchReceivedGifts = async () => {
+        const localStorage: Storage = window.localStorage;
+        const token = localStorage.getItem("accessToken");
+        try {
+          const response = await fetch("https://withsports.shop:8000/gifticon-service/order/gifticon/list/received",
+            {
+              method: "GET",
+              headers: {
+                Credentials: "include",
+                ContentType: "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
 
-    //       const data = await response.json();
-    //       setReceivedGifts(data.data.orders); // 받은 기프티콘 목록을 상태에 저장
-    //     } catch (error) {
-    //       console.error("Error:", error);
-    //     }
-    //   };
-    //   fetchReceivedGifts();
-    // }, []);
+          const data = await response.json();
+          setReceivedGifts(data.data.orders); // 받은 기프티콘 목록을 상태에 저장
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      };
+      fetchReceivedGifts();
+    }, []);
   
     
-    // const openDetailModal  = (gift: ReceivedGiftInfo) => {
-    //     setSelectedGift(gift);
-    //     setModalOpen(true);
-    // };
+    const openDetailModal  = (gift: ReceivedGiftInfo) => {
+        setSelectedGift(gift);
+        setModalOpen(true);
+    };
   
-    // return (
-    //   <div>
-    //     <h1>받은 기프티콘</h1>
-    //     <table>
-    //       <thead>
-    //         <tr>
-    //           <th>기프티콘</th>
-    //           <th>Serial Num.</th>
-    //           <th>사용 여부</th>
-    //         </tr>
-    //       </thead>
-    //       <tbody>
-    //         {receivedGifts.map((gift) => (
-    //         <tr key={gift.gifticonId} onClick={() => openDetailModal(gift)}>
-    //             <td>{gift.gifticonName}</td>
-    //             <td>{gift.serialNumber}</td>
-    //             <td>{gift.used ? "사용완료" : "사용가능"}</td>
-    //         </tr>
-    //         ))}
-    //         </tbody>
-    //     </table>
-    //     {/* 상품 상세 화면 모달창 */}
-    //     {modalOpen && selectedGift && (
-    //     <div
-    //     style={{
-    //         display: "flex",
-    //         justifyContent: "center",
-    //         alignItems: "center",
-    //         position: "fixed",
-    //         top: 0,
-    //         right: 0,
-    //         bottom: 0,
-    //         left: 0,
-    //         backgroundColor: "rgba(0, 0, 0, 0.5)",
-    //     }}
-    //     >
-    //     <form
-    //         className={styles["modal-form"]}
-    //         style={{
-    //         display: "flex",
-    //         flexDirection: "column",
-    //         alignItems: "center",
-    //         justifyContent: "center",
-    //         backgroundColor: "white",
-    //         borderRadius: "10px",
-    //         padding: "20px",
-    //         width: "300px",
-    //         }}>
-    //         {/* 바코드 표시*/}
-    //         <Image  
-    //             src="/barcode.jpg"
-    //             width={200}
-    //             height={50}
-    //             alt ="barcode"
-    //         />
-    //         <h2>{selectedGift.gifticonName}</h2>
-    //         <p>보낸 사람: {selectedGift.toUserNickname}</p>
-    //         <p>Serial Number: {selectedGift.serialNumber}</p>
-    //         <p>메시지 : {selectedGift.letter}</p>
-    //         <p>사용 여부 {selectedGift.used ? "사용완료" : "사용가능"}</p>
-    //         <button type="button" onClick={() => {setModalOpen(false);}} className={styles.backbutton}>닫기</button>
-    //       </form>
-    //       </div>
-    //   )}
-    //   </div>
-    // );
+    return (
+      <div>
+        <h1>받은 기프티콘</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>기프티콘</th>
+              <th>Serial Num.</th>
+              <th>사용 여부</th>
+            </tr>
+          </thead>
+          <tbody>
+            {receivedGifts.map((gift) => (
+            <tr key={gift.gifticonId} onClick={() => openDetailModal(gift)}>
+                <td>{gift.gifticonName}</td>
+                <td>{gift.serialNumber}</td>
+                <td>{gift.used ? "사용완료" : "사용가능"}</td>
+            </tr>
+            ))}
+            </tbody>
+        </table>
+        {/* 상품 상세 화면 모달창 */}
+        {modalOpen && selectedGift && (
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "fixed",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            }}
+        >
+        <div style={{ 
+            display: "flex", 
+            flexDirection: "column", 
+            alignItems: "center", 
+            justifyContent: "center", 
+            backgroundColor: "white", 
+            borderRadius: "10px", 
+            padding: "20px", 
+            width: "300px" 
+        }}>
+            {/* 바코드 표시*/}
+            <Image  
+                src="/barcode.jpg"
+                width={200}
+                height={50}
+                alt ="Barcode"
+            />
+            <h2>{selectedGift.gifticonName}</h2>
+            <p>보낸 사람: {selectedGift.toUserNickname}</p>
+            <p>Serial Number: {selectedGift.serialNumber}</p>
+            <p>메시지 : {selectedGift.letter}</p>
+            <p>사용 여부 {selectedGift.used ? "사용완료" : "사용가능"}</p>
+            <button type="button" onClick={() => {setModalOpen(false);}} className={styles.backbutton}>닫기</button>
+          </div>
+          </div>
+      )}
+      </div>
+    );
 }
