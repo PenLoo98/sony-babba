4ce51573a2d7840b5fe4a56d050115c4c6c7fcec
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-11-23 13:19:16 +0900

    Style (align homeButton Text)

--- src/components/Headers/HomeButton.tsx
+++ src/components/Headers/HomeButton.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function HomeButton() {
   return (
-    <div className="homeButton">
+    <div className="homeButton" style={{display: "flex", alignItems: "center"}}>
       <Link href="/" style={{ textDecoration: "none" }}>
         <h2 style={{ color: "#85f3ef",textShadow: "-1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000",padding: "10px", }}>With Sports</h2>
       </Link>
