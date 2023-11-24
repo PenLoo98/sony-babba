5604f777385a92616c79bdef020777532ef57adb
JokerBear_96 <9619pjw@gmail.com>
2023-11-24 16:23:27 +0900

    fix2 admin user page

--- src/app/admin/user/profile/[id]/page.tsx
+++ src/app/admin/user/profile/[id]/page.tsx
@@ -11,24 +11,17 @@ type PageParams = {
 type UserJSON = {
   userId: number;
   nickname: string;
-  introduction: string;
+  introduction: string | null;
   area: string;
-  imageUrl: string;
-  tier: string;
+  imageUrl: string | null;
+  tier?: string;
+  rating?: number;
   win: number;
   lose: number;
   draw: number;
-  winRate: number;
-  mvpCount: number;
-  teamName?: string;
+  winRate: number | undefined | null;
 };
 export default function ProfilePage({ params }: { params: PageParams }) {
-  const getUserInfoURL: string = `https://withsports.shop:8000/user-service/user/profile/${params.id}`;
-
-  // 로컬스토리지 토큰 가져오기
-  const localStorage: Storage = window.localStorage;
-  const token = localStorage.getItem("accessToken");
-
   // 팀 정보를 불러왔는지 여부
   const [showUserInfo, setShowUserInfo] = useState(false);
 
@@ -39,78 +32,87 @@ export default function ProfilePage({ params }: { params: PageParams }) {
     area: "서울",
     imageUrl: "/default-profile.png",
     tier: "1",
+    rating: 1000,
     win: 5,
     lose: 5,
     draw: 0,
     winRate: 50,
-    mvpCount: 5,
-    teamName: "손이바빠",
   };
   const [data, setData] = useState<UserJSON>(userJSON);
 
   // TODO: GET - id에 맞는 사용자 정보 가져오기
-  async function getUserInfo(getUserInfoURL: string) {
+  async function getUserInfo() {
+    // 로컬스토리지 토큰 가져오기
+    const localStorage: Storage = window.localStorage;
+    const token = localStorage.getItem("accessToken");
+    let userId = params.id;
+
+    // 요청 URL - PathVariable 없는 본인 프로필 조회
+    // const getUserInfoURL: string = `https://withsports.shop:8000/user-service/user/profile`;
+
+    // 요청 URL - PathVariable: userId
+    const getUserInfoURL: string = `https://withsports.shop:8000/user-service/user/other/profile/${userId}`;
+
     const response = await fetch(getUserInfoURL, {
       method: "GET",
       headers: {
-        Credentials: "include",
+        "Credentials": "include",
         "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
+        "Authorization": `Bearer ${token}`,
       },
     })
       .then((res) => res.json())
       .then((data) => {
+        console.log("data:");
+        console.log(data);
         if (data.code === "SUCCESS") {
           console.log("사용자 정보를 불러오는데 성공했습니다.");
         } else {
           console.log("사용자 정보를 불러오는데 실패했습니다.");
         }
-        return data.data;
+        return data;
       })
       .catch((error) => {
         console.log(error);
         throw new Error("서버 요청 실패!");
       });
 
-      // 응답값 확인 
-      let body: UserJSON;
-      if(response){
-        body = await response.data;
-        console.log("body:");
-        console.log(body);
-        return body;
-      } else {
-        console.log("응답이 없습니다.");
-        return null;
-      }
+    // 응답값 확인
+    let body: UserJSON;
+    if (response) {
+      // data부분만 가져오기
+      body = await response.data;
+      console.log("body:");
+      console.log(body);
+      return body;
+    } else {
+      console.log("응답이 없습니다.");
+      return null;
+    }
   }
 
   useEffect(() => {
     async function fetchUserData() {
-      let userInfo = await getUserInfo(getUserInfoURL);
+      let userInfo = await getUserInfo();
       if (userInfo) {
         console.log("userInfo:");
         console.log(userInfo);
-        setShowUserInfo(true);
         setData(userInfo);
         console.log("data:");
         console.log(data);
-        console.log("data.imageUrl:");
-        console.log(data.imageUrl);
       }
-      
     }
     fetchUserData();
     setShowUserInfo(true);
-  },[]);
+  }, []);
 
   return (
     <div>
       {!showUserInfo && <div>유저 정보를 불러오는 중입니다...</div>}
       {showUserInfo && (
         <div>
-          <ProfileInfo userJSON={data}/>
-          <ProfileMenu userJSON={data}/>
+          <ProfileInfo userJSON={data} />
+          <ProfileMenu pageId={params.id} userJSON={data} />
           <RankingInfo userJSON={data} />
         </div>
       )}
